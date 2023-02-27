a8b46eb38de7180e3261a9cceda889e820244dcf
varma01 <varma02.marci@gmail.com>
2023-02-27 09:26:10 +0100

    changes to main
    - fixed window size
    - added button keys
    - added exit event

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import PySimpleGUI as sg
 
 menu_layout = [
-	[sg.Button("Foglalás rögzítése", expand_x=True), ],
-	[sg.Button("Foglalás törlése", expand_x=True), ],
-	[sg.Button("Statisztika", expand_x=True), ],
-	[sg.Button("Kilépés", expand_x=True), ],
+	[sg.Button("Foglalás rögzítése", key="-REC-", expand_x=True), ],
+	[sg.Button("Foglalás törlése", key="-DEL-", expand_x=True), ],
+	[sg.Button("Statisztika", key="-STAT-", expand_x=True), ],
+	[sg.Button("Kilépés", key="-EXIT-", expand_x=True), ],
 ]
 
-window = sg.Window('Foglalás Rögzítése', menu_layout, resizable=True, finalize=True)
-window.set_min_size((200, 140))
+window = sg.Window('Foglalás - MENÜ', menu_layout, resizable=False, size=(200, 140))
 
 while True:
 	event, values = window.read()
 	print(event, values)
-	if event == sg.WIN_CLOSED:
-		break
+
+	match event:
+		case sg.WIN_CLOSED | "-EXIT-": break
 
 window.close()
