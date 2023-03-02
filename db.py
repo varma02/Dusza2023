c1329860eb05faceb571ae92c8f5bb1739d938d6
varma02 <varma02.marci@gmail.com>
2023-03-02 09:42:35 +0100

    finish db

--- db.py
+++ db.py
@@ -35,7 +35,7 @@ def _read_db(year:int) -> list[Record]:
 			))
 	return records
 
-def _append_db(*records:Record, year:int):
+def append_db(*records:Record, year:int):
 	path = f"{DATA_DIR}/foglalasok/{year}.txt"
 	open(path, "w", encoding="utf-8").close()
 	with open(path, "a", encoding="utf-8") as f:
@@ -84,5 +84,34 @@ def get_records(year:int=None, filter_canceled=False, filter_today=False):
 def get_years() -> list[int]:
 	return list(map(lambda x: int(x[:-4]), glob.glob("*.txt", root_dir=f"{DATA_DIR}/foglalasok/", recursive=False)))
 
-def add_records(*records:Record):
-	raise Exception("TODO: IMPLEMENT NEW RECORD")
+def reserve_table(name:str, start:datetime, end:datetime, chairs:int, type:str) -> list[Table] | bool:
+	""" Returns a list of tables to be reserved. Or if there are not enough of tables, it returns an empty list and if the person alredy reserved in this interval, it returns false.
+	"""
+	records = get_records(start.year)
+	intersecting_records:list[Record] = []
+	for r in records:
+		if max(r.start, start) <= min(r.end, end):
+			intersecting_records.append(r)
+
+	for r in intersecting_records:
+		if r.name == name:
+			return False
+
+	reserved_tables = [t for r in intersecting_records for t in r.tables]
+	
+	tables = [x for x in get_tables() if x not in reserved_tables and x.type == type]
+	tables.sort(key=lambda x: x["chairs"])
+
+	reserving = []
+
+	while chairs > 0 and len(tables) > 0:
+		if tables[0].chairs >= chairs:
+			reserving.append(tables[0])
+			return reserving
+		else:
+			table1 = tables.pop(0)
+			table2 = tables.pop(0)
+			chairs -= table1.chairs + table2.chairs - 2
+			reserving.append(table1, table2)
+	
+	return []
