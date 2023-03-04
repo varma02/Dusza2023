00c32e3d2b318adc9e0b5c1198399b2228ab1fb3
varma02 <varma02.marci@gmail.com>
2023-03-04 09:50:37 +0100

    update del_record.py

--- del_records.py
+++ del_records.py
@@ -2,42 +2,65 @@ import PySimpleGUI as sg
 from datetime import datetime
 import db
 
-def run():
-	reca = db.get_records(datetime.now().year, filter_canceled=False, filter_today=False)
-	rndr = []
-	for Item in reca: 
-		dct = Item.__dict__
-		print(dct)
-		dt = dct["start"]
-		rndr.append([
-			sg.Text(dct["name"], font=("Arial", 14)),
-			sg.Text(" "),
-			sg.Text(f"{dt.strftime('%m. %d. %H:%M')}", font=("Arial", 11)),
-			sg.Text("", expand_x=True),
-			sg.Button("Törlés", button_color=('white', "red"), key="-SAVE-", font=("Arial", 12)),
-        ])
-      
+def generate_layout(year, query:str = None):
+	layout = []
+	records = db.get_records(year, filter_canceled=False, filter_today=False)
+	for index, item in enumerate(records): 
+		if (not query) or (item.name.lower().strip().find(query.lower().strip()) != -1):
+			layout.append([
+				sg.Text(item.name, font=("Arial", 11, "bold")),
+				sg.Text(f"{item.start.strftime('%Y/%m/%d %H:%M')} - {item.end.strftime('%H:%M')}", expand_x=True, font=("Arial", 11)),
+				sg.Button("Lemondás", button_color=('white', "red"), key=f"-DEL-{index}-", font=("Arial", 12)),
+			])
 
-	window = sg.Window('Foglalás - Törlés', [
-		[sg.Column(
-            layout=rndr,
-            scrollable=True,
-	        vertical_scroll_only=True,
-            expand_x=True,
-            expand_y=True
-        ),]
-	], resizable=False, size=(600, 200))
-	
+	return [
+		[
+			sg.Text("Foglalás lemondása", font=('Arial', 18), expand_x=True),
+			sg.Input(query, key="-QUERY-", enable_events=True, size=(15, None)),
+			sg.InputCombo(db.get_years(), year, key="-YEAR-", enable_events=True),
+			sg.Button("Keresés", key="-SEARCH-"),
+		],[sg.Column(
+			layout=layout,
+			scrollable=True,
+			vertical_scroll_only=True,
+			expand_x=True,
+			expand_y=True,
+			key="-LIST-",
+		),]
+	]
+
+
+def create_window(year, query):
+	window = sg.Window(
+		title = 'Foglalás - Lemondás', 
+		layout = generate_layout(year, query), 
+		resizable = True, 
+		size = (600, 300))
 	window.finalize()
+	window.set_min_size((470, 150))
+	return window
+
+
+
+def run():    
+	query = ""
+	year = datetime.now().year
+	window = create_window(year, query)
 
 	while True:
 		event, values = window.read()
 		print(event, values)
 
 		match event:
+			case "-SEARCH-":
+				window.close()
+				window = create_window(year, query)
+			case "-QUERY-": query = values["-QUERY-"]
+			case "-YEAR-": year = values["-YEAR-"]
 			case sg.WIN_CLOSED | "-EXIT-": break
-				
-					
+
+		if event.startswith("-DEL-"):
+			raise Exception("TODO: Implement record deletion")
 
 	window.close()
 
